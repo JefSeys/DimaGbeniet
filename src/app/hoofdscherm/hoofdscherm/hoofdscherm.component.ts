0c2b25a89c73edd3f98e13c7a04d918078241026
Jef Seys <jef.seys@gmail.com>
2021-04-16 16:41:54 +0200

    Style

--- src/app/hoofdscherm/hoofdscherm/hoofdscherm.component.ts
+++ src/app/hoofdscherm/hoofdscherm/hoofdscherm.component.ts
@@ -111,6 +111,45 @@ export class HoofdschermComponent implements OnInit {
     })
   ];
 
+  imagesDetail: Image[] = [
+    new Image(
+      0,
+      {
+        img: 'assets/images/a.jpg'
+      },
+      {
+        img: 'assets/images/a.jpg'
+      }
+    ),
+    new Image(
+      1,
+      {
+        img: 'assets/images/b.jpg'
+      },
+      {
+        img: 'assets/images/b.jpg'
+      }
+    ),
+    new Image(
+      2,
+      {
+        img: 'assets/images/c.jpg'
+      },
+      {
+        img: 'assets/images/c.jpg'
+      }
+    ),
+    new Image(
+      3,
+      {
+        img: 'assets/images/d.jpg'
+      },
+      {
+        img: 'assets/images/d.jpg'
+      }
+    )
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
